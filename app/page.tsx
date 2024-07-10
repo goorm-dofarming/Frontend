7a63b7d53e1d5a8aeabc49342f5695bc44883f7e
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-07-10 23:20:42 +0900

    ci cd 적용

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          12233 Get started by editing&nbsp;
+          1223 Get started by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
         <div>
