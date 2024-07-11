042dc7cc23ece5d113540b76c8043061037e9bcb
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-07-11 19:21:52 +0900

    ci cd 적용 deploy.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,13 @@
 REPOSITORY=/home/ubuntu/deploy
 # pm2의 전체 경로 설정
 PM2_BIN="/home/ubuntu/.nvm/versions/node/v20.15.1/bin/pm2"
-set -e
-
-echo "Starting deploy script..."
 
 cd $REPOSITORY
 
 sudo pnpm install
 
-sudo $PM2_BIN resurrect
+sudo pm2 resurrect
 
-sudo $PM2_BIN reload all
+sudo pm2 reload all
 
 echo "Deploy script completed."
