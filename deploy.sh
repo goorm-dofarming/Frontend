dfd209fd8af04aa5300fa8e5dd03259880520410
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-07-11 15:21:11 +0900

    ci cd 적용 deploy.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ PM2_BIN="/home/ubuntu/.nvm/versions/node/v20.15.1/bin/pm2"
 # pm2가 실행 가능한지 확인
 if [ -x "$PM2_BIN" ]; then
     # pm2를 사용하여 애플리케이션 시작
-    $PM2_BIN start Frontend
+    sudo $PM2_BIN start pnpm -w Frontend -- start
 else
     echo "pm2를 찾거나 실행할 수 없습니다: $PM2_BIN"
     exit 1
