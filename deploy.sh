ded5fdad3ec6f5a97127d5908b579df6ac4d0f60
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-07-11 16:05:14 +0900

    ci cd 적용 deploy.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 REPOSITORY=/home/ubuntu/deploy
+# pm2의 전체 경로 설정
+PM2_BIN="/home/ubuntu/.nvm/versions/node/v20.15.1/bin/pm2"
 
 cd $REPOSITORY
 
 sudo pnpm install
-# pm2의 전체 경로 설정
-PM2_BIN="/home/ubuntu/.nvm/versions/node/v20.15.1/bin/pm2"
+
 sudo $PM2_BIN resurrect
 
 sudo $PM2_BIN reload all
