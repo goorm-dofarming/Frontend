a0936aac4bf7145f4a30bb4813e0c77f04d8045d
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-07-12 00:27:26 +0900

    ci cd 적용 오류 수정

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,9 @@
 REPOSITORY=/home/ubuntu/deploy
-# pm2의 전체 경로 설정
-PM2_BIN="/home/ubuntu/.nvm/versions/node/v20.15.1/bin/pm2"
 
 cd $REPOSITORY
 
-sudo pnpm install
+sudo npm install
 
 sudo pm2 resurrect
 
-sudo pm2 reload all
-
-echo "Deploy script completed."
+sudo pm2 start all
