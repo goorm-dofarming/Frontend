1dc1836758a9279e340473e60201c5489dbebc03
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-07-12 15:38:01 +0900

    7/12 ci cd 적용

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,6 @@ REPOSITORY=/home/ubuntu/deploy
 
 cd $REPOSITORY
 
-# Remove existing problematic file if it exists
-rm -f /home/ubuntu/deploy/node_modules/.pnpm/eslint-config-next@14.2.4_eslint@8.57.0_typescript@5.5.3/node_modules/typescript/lib/zh-cn/diagnosticMessages.generated.json
-
 sudo pnpm install
 
 sudo pm2 resurrect
