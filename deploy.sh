e98f6ccd8255485fa6b0769a36965a78c86e1fe4
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-07-12 19:21:19 +0900

    7/12 ci cd 적용

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,7 @@ cd $REPOSITORY
 # Remove existing problematic file if it exists
 rm -f /home/ubuntu/deploy/node_modules/.pnpm/eslint-config-next@14.2.4_eslint@8.57.0_typescript@5.5.3/node_modules/typescript/lib/zh-cn/diagnosticMessages.generated.json
 
+sudo pnpm install
+
 # Start or restart the application
-sudo pm2 start app.js --name Frontend || sudo pm2 restart Frontend
-
-# Save the pm2 process list
-sudo pm2 save
-
-# Reload all pm2 processes
-sudo pm2 reload all
+sudo pm2 start npm --name "dofarming" -- start -- -p 4000
