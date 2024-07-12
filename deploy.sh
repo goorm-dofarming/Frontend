4b94f1d05881724128552a3d6f90f00bb21542d9
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-07-12 17:32:18 +0900

    7/12 ci cd 적용

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,6 @@ cd $REPOSITORY
 # Remove existing problematic file if it exists
 rm -f /home/ubuntu/deploy/node_modules/.pnpm/eslint-config-next@14.2.4_eslint@8.57.0_typescript@5.5.3/node_modules/typescript/lib/zh-cn/diagnosticMessages.generated.json
 
-# Install dependencies
-sudo pnpm install
-
 # Start or restart the application
 sudo pm2 start app.js --name Frontend || sudo pm2 restart Frontend
 
