89dde2aeed40d9115c48554ece922156a0a747e2
NS0H <bory1610@gmail.com>
2024-07-17 17:06:07 +0900

    style: Modal 위치 및 배경 수정

--- app/_components/Common/Modal.tsx
+++ app/_components/Common/Modal.tsx
@@ -1,4 +1,6 @@
-import React, { useContext, useState } from 'react';
+'use client';
+import React, { useContext, useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
 import Image from 'next/image';
 
 // img
@@ -21,23 +23,37 @@ const Modal: React.FC<MainModalType> = ({
   openModal,
   modal,
 }) => {
-  return (
+  const [modalRoot, setModalRoot] = useState<HTMLElement | null>(null);
+
+  useEffect(() => {
+    const root = document.querySelector(
+      '#modal-container'
+    ) as HTMLElement | null;
+    setModalRoot(root);
+  }, []);
+
+  if (!modalRoot) return null;
+
+  return ReactDOM.createPortal(
     <ModalContainer $modal={modal} width={width} height={height}>
-      {/* 모달 헤더 */}
-      <div className="modalHeader">
-        <Image
-          className="logo"
-          src={CloseBtn}
-          alt="닫기"
-          width={30}
-          height={30}
-          onClick={openModal}
-        />
+      <div className="modal">
+        {/* 모달 헤더 */}
+        <div className="modalHeader">
+          <Image
+            className="logo"
+            src={CloseBtn}
+            alt="닫기"
+            width={30}
+            height={30}
+            onClick={openModal}
+          />
+        </div>
+        {/* 모달 컨텐츠 부분 */}
+        {children}
+        {/*  */}
       </div>
-      {/* 모달 컨텐츠 부분 */}
-      {children}
-      {/*  */}
-    </ModalContainer>
+    </ModalContainer>,
+    modalRoot
   );
 };
 
