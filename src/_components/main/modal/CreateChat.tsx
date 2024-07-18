52a4f33b7f8bbded54e7310b6a0d2838b52b054d
NS0H <bory1610@gmail.com>
2024-07-18 16:47:47 +0900

    feat: 채팅 로딩, 에러 화면 구현, style: 기타 CSS 수정

--- src/_components/main/modal/CreateChat.tsx
+++ src/_components/main/modal/CreateChat.tsx
@@ -113,21 +113,21 @@ const CreateChat: React.FC<CreateChatProps> = ({ openModal, onCreateChat }) => {
           onChange={handleRegionChange}
         >
           <option value="" disabled hidden></option>
-          <option value="SEOUL">서울특별시</option>
-          <option value="INCHEON">인천광역시</option>
-          <option value="ULSAN">울산광역시</option>
-          <option value="BUSAN">부산광역시</option>
-          <option value="GWANGJU">광주광역시</option>
-          <option value="DAEJEON">대전광역시</option>
-          <option value="GYEONGGI">경기도</option>
-          <option value="CHUNGBUK">충청북도</option>
-          <option value="CHUNGNAM">충청남도</option>
-          <option value="GYEONGNAM">경상남도</option>
-          <option value="GYEONGBUK">경상북도</option>
-          <option value="JEONNAM">전라남도</option>
-          <option value="JEONBUK">전라북도</option>
-          <option value="JEJU">제주특별자치도</option>
-          <option value="GANGWON">강원특별자치도</option>
+          <option value="서울특별시">서울특별시</option>
+          <option value="인천광역시">인천광역시</option>
+          <option value="울산광역시">울산광역시</option>
+          <option value="부산광역시">부산광역시</option>
+          <option value="광주광역시">광주광역시</option>
+          <option value="대전광역시">대전광역시</option>
+          <option value="경기도">경기도</option>
+          <option value="충청남도">충청북도</option>
+          <option value="충청북도">충청남도</option>
+          <option value="경상남도">경상남도</option>
+          <option value="경상북도">경상북도</option>
+          <option value="전라남도">전라남도</option>
+          <option value="전라북도">전라북도</option>
+          <option value="제주특별자치도">제주특별자치도</option>
+          <option value="강원특별자치도">강원특별자치도</option>
         </select>
       </div>
       <div className={styles.blank} />
