228c35d81d3b4fee6b464840c99a74ac0c6ce87d
Jinju Seo <wjbbdq1223@naver.com>
2024-07-18 15:33:18 +0900

    feat: 파비콘, 타이틀 재적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
-import { RecoilProvider } from '@/src/utils/recoilProvider';
-import './globals.css';
-import type { AppProps } from 'next/app';
-import { ReactQueryProvider } from '@/src/utils/queryProvider';
-import { MSWComponent } from '@/src/mocks/MSWComponent';
+import { RecoilProvider } from "@/src/utils/recoilProvider";
+import "./globals.css";
+import type { AppProps } from "next/app";
+import { ReactQueryProvider } from "@/src/utils/queryProvider";
+import { MSWComponent } from "@/src/mocks/MSWComponent";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const enableMSW = process.env.NEXT_PUBLIC_ENABLE_MSW === 'false';
+  const enableMSW = process.env.NEXT_PUBLIC_ENABLE_MSW === "false";
   return (
     <RecoilProvider>
       <ReactQueryProvider>
+        <Head>
+          <title>Dofarming</title>
+          <meta property="og:title" content="Dofarming" key="title" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
         {enableMSW ? (
           <MSWComponent enableMSW={enableMSW}>
             <Component {...pageProps} />
