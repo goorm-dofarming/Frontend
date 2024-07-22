f362f2dc55a5cb9d43b5aae0b4972a7bb3e1fe19
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-07-22 16:30:00 +0900

    docs: 2차 배포

--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,19 @@ cd $REPOSITORY
 
 # Remove existing problematic file if it exists
 if [ -f "$REPOSITORY/node_modules/.pnpm/eslint-config-next@14.2.4_eslint@8.57.0_typescript@5.5.3/node_modules/typescript/lib/zh-cn/diagnosticMessages.generated.json" ]; then
-  rm -f $REPOSITORY/node_modules/.pnpm/eslint-config-next@14.2.4_eslint@8.57.0_typescript@5.5.3/node_modules/typescript/lib/zh-cn/diagnosticMessages.generated.json
+  rm -f "$REPOSITORY/node_modules/.pnpm/eslint-config-next@14.2.4_eslint@8.57.0_typescript@5.5.3/node_modules/typescript/lib/zh-cn/diagnosticMessages.generated.json"
 fi
 
 # Remove existing node_modules directory if it exists
 if [ -d "$REPOSITORY/node_modules" ]; then
-  rm -rf $REPOSITORY/node_modules
+  sudo rm -rf "$REPOSITORY/node_modules"
 fi
 
+# Clear pnpm store cache
+sudo pnpm store prune
+
 # Install packages
-sudo pnpm install
+sudo pnpm install --force
 
 # Start or restart the application
 sudo pm2 start npm --name "dofarming" -- start -- -p 4000
