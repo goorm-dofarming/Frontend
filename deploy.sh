ca1880fd7a6d2f5233607a043af2d9062e258575
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-07-22 16:16:30 +0900

    docs: 2차 배포

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
+set -e
 
 REPOSITORY=/home/ubuntu/deploy
 
 cd $REPOSITORY
 
 # Remove existing problematic file if it exists
-rm -f /home/ubuntu/deploy/node_modules/.pnpm/eslint-config-next@14.2.4_eslint@8.57.0_typescript@5.5.3/node_modules/typescript/lib/zh-cn/diagnosticMessages.generated.json
+if [ -f "$REPOSITORY/node_modules/.pnpm/eslint-config-next@14.2.4_eslint@8.57.0_typescript@5.5.3/node_modules/typescript/lib/zh-cn/diagnosticMessages.generated.json" ]; then
+  rm -f $REPOSITORY/node_modules/.pnpm/eslint-config-next@14.2.4_eslint@8.57.0_typescript@5.5.3/node_modules/typescript/lib/zh-cn/diagnosticMessages.generated.json
+fi
 
+# Remove existing node_modules directory if it exists
+if [ -d "$REPOSITORY/node_modules" ]; then
+  rm -rf $REPOSITORY/node_modules
+fi
+
+# Install packages
 sudo pnpm install
 
 # Start or restart the application
