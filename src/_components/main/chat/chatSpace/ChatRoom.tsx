68297d7d30fb586f7f52771e3344dcbef5fab2c5
NS0H <bory1610@gmail.com>
2024-07-29 15:06:24 +0900

    feat: 채팅방 프로필 이미지 구현, 날짜 구분선 및 시간 표시, 안읽은 채팅으로 스크롤 이동

--- src/_components/main/chat/chatSpace/ChatRoom.tsx
+++ src/_components/main/chat/chatSpace/ChatRoom.tsx
@@ -12,7 +12,7 @@ import Profile from '@/src/_assets/main/defaultProfile.svg';
 
 // atom
 import { useRecoilState } from 'recoil';
-import { userState } from '@/src/atom/stats';
+import { selectedChatState, userState } from '@/src/atom/stats';
 
 // types
 import { Message } from '@/src/types/aboutChat';
@@ -22,23 +22,57 @@ import axios from 'axios';
 import { getUser } from '@/pages/api/user';
 import Landing from '@/src/_components/Common/Landing';
 
-// 메세지 검색해서 가져오고 웹 소켓 연결하고 룸 구독
-// room/{roomId}
-// chat/sendmessage
-
 const ChatRoom: React.FC<{
   messageQuery: QueryObserverResult<Message[], Error>;
-}> = ({ messageQuery }) => {
+  unreadMessageCount: number;
+}> = ({ messageQuery, unreadMessageCount }) => {
   const { data: messages = [], error, isLoading } = messageQuery;
   const [user] = useRecoilState(userState);
   const [usersImage, setUsersImage] = useState<{
     [key: number]: string | null;
   }>({});
   const prevDateRef = useRef<Date | null>(null);
+  const [selectedChat, setSelectedChat] = useRecoilState(selectedChatState);
 
-  // 유저 이미지는 없거나 undifined
-  // 기본 이미지 이거나 null
-  // 존재할 수 있다 imageUrl
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [moveScroll, setMoveScroll] = useState<boolean>(true);
+  const targetRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (unreadMessageCount > 0) {
+      setMoveScroll(true);
+      // console.log('scroll true');
+    }
+  }, [unreadMessageCount]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (targetRef.current && containerRef.current && moveScroll) {
+        targetRef.current.scrollIntoView({ behavior: 'smooth' });
+        // console.log('이동');
+      }
+    }, 200);
+  }, [selectedChat, moveScroll]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (containerRef.current && containerRef.current.scrollTop === 0) {
+        setMoveScroll(false);
+        // console.log('scroll false');
+      }
+    };
+
+    const currentContainer = containerRef.current;
+    if (currentContainer) {
+      currentContainer.addEventListener('scroll', handleScroll);
+    }
+
+    return () => {
+      if (currentContainer) {
+        currentContainer.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, []);
 
   const getImageUrl = (url: string | null) => {
     if (url && !url.startsWith('http') && !url.startsWith('https')) {
@@ -55,7 +89,7 @@ const ChatRoom: React.FC<{
 
   const getDate = (date: Date) => {
     const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 1을 더해줌
+    const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
 
     return `${year}년 ${month}월 ${day}일`;
@@ -63,14 +97,13 @@ const ChatRoom: React.FC<{
 
   const getHour = (data: string) => {
     const date = new Date(data);
-
     date.setHours(date.getHours() + 9);
 
     const hours = date.getHours();
     const minutes = String(date.getMinutes()).padStart(2, '0');
 
     const period = hours < 12 ? '오전' : '오후';
-    const displayHours = hours % 12 || 12; // 0시, 12시 구분
+    const displayHours = hours % 12 || 12;
     return `${period} ${displayHours}시 ${minutes}분`;
   };
 
@@ -110,7 +143,7 @@ const ChatRoom: React.FC<{
   }
 
   return (
-    <div className={styles.chatRoom}>
+    <div className={styles.chatRoom} ref={containerRef}>
       {messages.length > 0 &&
         user &&
         messages.map((message, index) => {
@@ -184,6 +217,9 @@ const ChatRoom: React.FC<{
                   <div className={styles.date}>{getDate(messageDate)}</div>
                   <hr />
                 </div>
+              )}
+              {index === unreadMessageCount - 1 && moveScroll && (
+                <div ref={targetRef}></div>
               )}
             </div>
           );
