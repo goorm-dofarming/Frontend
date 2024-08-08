17c5516cbede8a2827b03468aaaf653040f54a5c
NS0H <bory1610@gmail.com>
2024-08-08 11:45:36 +0900

    fix: 로그 스크롤바 포커스로 이동

--- src/_components/main/Log.tsx
+++ src/_components/main/Log.tsx
@@ -6,7 +6,7 @@ import { LogContainer } from '@/src/_styles/main/logStyles';
 // libraries
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { useRecoilState, useRecoilValue } from 'recoil';
-import { logEntireData, pageState ,logMapState} from '@/src/atom/stats';
+import { logEntireData, pageState, logMapState } from '@/src/atom/stats';
 
 // types
 import { logDataType, recommendsType } from '@/src/types/aboutLog';
@@ -47,29 +47,32 @@ const Log = () => {
   const [logData, setLogData] = useRecoilState<logDataType[]>(logEntireData);
 
   // 선택된 로그 데이터들
-  const [selectedLogData, setSelectedLogData] = useState<recommendsType[]>([
-  ]);
+  const [selectedLogData, setSelectedLogData] = useState<recommendsType[]>([]);
+  const [selectedLocationId, setSelectedLocationId] = useState<number>(0);
   const [pins, setPins] = useState<any[]>([]);
   const [infoWindows, setInfoWindows] = useState<any[]>([]);
-  const [markers,setMarkers] = useState<any[]>([]);
+  const [markers, setMarkers] = useState<any[]>([]);
+
   // 로그 아이디
   const [logId, setLogId] = useState<number>(0);
 
   // 카드 클릭 시 정보 모달
   const [modal, setModal] = useState<boolean>(false);
   const openModal = useToggle(modal, setModal);
-  const [selectedLocationId, setSelectedLocationId] = useState<number>(0);
+
+  // 로그 스크롤바
+  const logRefs = useRef<HTMLDivElement>(null);
 
   // 전체 로그 데이터 불러오기
 
-  const getLogs = async()=>{
+  const getLogs = async () => {
     const response = await getLog();
     // console.log("get logs", response.data);
-    const data= response.data;
+    const data = response.data;
     if (response.status === 200) {
       setLogData(data);
     }
-  }
+  };
   // const getLogs = useQuery({
   //   queryKey: ['getLogs'],
   //   queryFn: async () => {
@@ -86,33 +89,34 @@ const Log = () => {
   useEffect(() => {
     // getLogs.refetch();
     getLogs();
-  },[]);
+  }, []);
 
-  useEffect(()=>{
-    if(logData.length>0){
+  useEffect(() => {
+    if (logData.length > 0) {
       getLogSubData.mutate(logData[0].logId);
       setSelectedLogIndex(logData[0].logId);
     }
-  },[logData])
-  
+  }, [logData]);
+
   // 로그 하위 데이터 불러오기
   const getLogSubData = useMutation({
     mutationFn: async (logId: number) => {
-      if(logId===0){ return [];}
+      if (logId === 0) {
+        return [];
+      }
       const response = await getLogData(logId);
-       //console.log("hi", logId);
+      //console.log("hi", logId);
       // console.log('get log data!!', response.data);
       // console.log(response);
-      
+
       return response.data.recommendations;
     },
     onSuccess: (data) => {
       setSelectedLogData(data);
-      ;
     },
-    onError:()=>{
-      console.log("get log data error");
-    }
+    onError: () => {
+      console.log('get log data error');
+    },
   });
 
   // 이미지 URL을 문자열로 변환하는 함수
@@ -124,17 +128,14 @@ const Log = () => {
     }
   };
 
-
   // 카드 클릭 시 정보 모달
   const onClickCard = (locationId: number) => {
     setSelectedLocationId(locationId);
     openModal();
   };
 
-
-
   useEffect(() => {
-    if(selectedLogData.length===0){
+    if (selectedLogData.length === 0) {
       return;
     }
     // document.cookie = 'username=dofarming; SameSite=Strict; Secure';
@@ -155,75 +156,75 @@ const Log = () => {
         const center = new window.kakao.maps.LatLng(
           // location.latitude,
           // location.longitude
-          35.95, 128.125
+          35.95,
+          128.125
         );
         const options = {
           center,
           level: 13,
         };
-        let map:any;
+        let map: any;
 
-        if ( containerRef.current !== null && kakaoMap === null) {
-            //console.log("null!!!!!!!");
+        if (containerRef.current !== null && kakaoMap === null) {
+          //console.log("null!!!!!!!");
           const mapContainer = document.getElementById('logMapContainer');
           map = new window.kakao.maps.Map(mapContainer, options);
           map.setMaxLevel(13);
-          
-        }else{
-          map=kakaoMap;
+        } else {
+          map = kakaoMap;
           //console.log("nullXXXXXXXX!!!!!!!");
         }
-        
+
         if (!window.kakao.maps.MarkerClusterer) {
           console.log('MarkerClusterer 로드 실패');
           // return;
         }
-        if(markers.length===0){
-        const clusterer = new window.kakao.maps.MarkerClusterer({
-          map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
-          averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
-          minLevel: 5, // 클러스터 할 최소 지도 레벨
-          ...clustererStyle,
-        });
-
-        const imageSrc = `http://${process.env.NEXT_PUBLIC_DEPLOY}/images/pin/pin_location.png`;
-        const imageSize = new window.kakao.maps.Size(36, 48); // 마커이미지의 크기입니다
-        const imageOption = {
-          offset: new window.kakao.maps.Point(0, 0),
-        }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-
-        const markerImage = new window.kakao.maps.MarkerImage(
-          imageSrc,
-          imageSize,
-          // imageOption
-        );
-
-        const new_markers = [];
-
-        for (let i = 0; i < logData.length; i++) {
-          let marker = new kakao.maps.Marker({
-            map: map, // 마커를 표시할 지도
-            position: new kakao.maps.LatLng(
-              Number(logData[i].latitude),
-              Number(logData[i].longitude)
-            ), // 마커를 표시할 위치
-            image: markerImage, // 마커 이미지
+        if (markers.length === 0) {
+          const clusterer = new window.kakao.maps.MarkerClusterer({
+            map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
+            averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
+            minLevel: 5, // 클러스터 할 최소 지도 레벨
+            ...clustererStyle,
           });
 
-          new_markers.push(marker);
+          const imageSrc = `http://${process.env.NEXT_PUBLIC_DEPLOY}/images/pin/pin_location.png`;
+          const imageSize = new window.kakao.maps.Size(36, 48); // 마커이미지의 크기입니다
+          const imageOption = {
+            offset: new window.kakao.maps.Point(0, 0),
+          }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
-          window.kakao.maps.event.addListener(
-            marker,
-            'click',
-            makeClickListener(logData[i])
+          const markerImage = new window.kakao.maps.MarkerImage(
+            imageSrc,
+            imageSize
+            // imageOption
           );
-        }
-        clusterer.addMarkers(new_markers);
-        setMarkers([...new_markers]);
-      }
 
-        if(pins.length > 0 && infoWindows.length>0){
-          for(let i=0;i<pins.length;i++){
+          const new_markers = [];
+
+          for (let i = 0; i < logData.length; i++) {
+            let marker = new kakao.maps.Marker({
+              map: map, // 마커를 표시할 지도
+              position: new kakao.maps.LatLng(
+                Number(logData[i].latitude),
+                Number(logData[i].longitude)
+              ), // 마커를 표시할 위치
+              image: markerImage, // 마커 이미지
+            });
+
+            new_markers.push(marker);
+
+            window.kakao.maps.event.addListener(
+              marker,
+              'click',
+              makeClickListener(logData[i])
+            );
+          }
+          clusterer.addMarkers(new_markers);
+          setMarkers([...new_markers]);
+        }
+
+        if (pins.length > 0 && infoWindows.length > 0) {
+          for (let i = 0; i < pins.length; i++) {
             pins[i].setMap(null);
             infoWindows[i].setMap(null);
           }
@@ -233,8 +234,8 @@ const Log = () => {
         for (let i = 0; i < selectedLogData.length; i++) {
           let imageSize = new kakao.maps.Size(36, 48);
 
-          let imageSrc =DataType[selectedLogData[i].dataType].img
-          
+          let imageSrc = DataType[selectedLogData[i].dataType].img;
+
           // 마커 이미지를 생성합니다
           let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
 
@@ -283,9 +284,9 @@ const Log = () => {
         }
         setPins([...new_pins]);
         setInfoWindows([...new_infoWindows]);
-        
+
         // 클러스터러에 마커들을 추가합니다
-        
+
         setKakaoMap(map);
       });
 
@@ -298,60 +299,76 @@ const Log = () => {
             latitude: Number(data.latitude),
             longitude: Number(data.longitude),
           });
-          
         };
       }
-      
     };
     script.addEventListener('load', onLoadKakaoMap);
-
-
   }, [selectedLogData]);
   useEffect(() => {
     if (kakaoMap === null) {
       return;
     }
-    if( location.latitude ===35.95 && location.longitude=== 128.25){
-        return;
-      }
-    const center = new window.kakao.maps.LatLng(location.latitude, location.longitude);
-    console.log("kakaoMap",kakaoMap);
+    if (location.latitude === 35.95 && location.longitude === 128.25) {
+      return;
+    }
+    const center = new window.kakao.maps.LatLng(
+      location.latitude,
+      location.longitude
+    );
+    console.log('kakaoMap', kakaoMap);
     kakaoMap.setLevel(6);
     kakaoMap.setCenter(center);
     kakaoMap.relayout();
-  }, [kakaoMap,location]);
+  }, [kakaoMap, location]);
+
+  // 스크롤바 이동
+  useEffect(() => {
+    if (selectedLogIndex) {
+      setTimeout(() => {
+        if (logRefs.current) {
+          logRefs.current.scrollIntoView({
+            behavior: 'smooth',
+            block: 'center',
+          });
+        }
+      }, 200);
+    }
+  }, [selectedLogIndex]);
   return (
     <LogContainer>
       <div className="logContainer">
         <h3>
-          <span>전체 기록</span>
+          <span>전체 기록 - {logData.length}개</span>
         </h3>
-        {logData.map((data, i) => (
-          <div
-            key={data.logId}
-            style={{ marginBottom: '0.4rem' }}
-            onClick={() => {
-              getLogSubData.mutate(data.logId);
-              setLocation({
-                latitude: Number(data.latitude),
-                longitude: Number(data.longitude),
-                level: 8,
-              });
-              setSelectedLogIndex(data.logId);
-              setLogId(data.logId);
-            }}
-          >
+        <div className="logs">
+          {logData.map((data, i) => (
             <div
-              className={`${selectedLogIndex ===  data.logId? 'log_selected' : 'log'}`}
+              key={data.logId}
+              style={{ marginBottom: '0.4rem' }}
+              onClick={() => {
+                getLogSubData.mutate(data.logId);
+                setLocation({
+                  latitude: Number(data.latitude),
+                  longitude: Number(data.longitude),
+                  level: 8,
+                });
+                setSelectedLogIndex(data.logId);
+                setLogId(data.logId);
+              }}
             >
-              <div className="logDate">{data.createdAt.split('T')[0]}</div>
-              <div className="logAddress">{data.address}</div>
-              <div className="logTheme">{data.theme}</div>
+              <div
+                className={`${selectedLogIndex === data.logId ? 'log_selected' : 'log'}`}
+                ref={selectedLogIndex === data.logId ? logRefs : null}
+              >
+                <div className="logDate">{data.createdAt.split('T')[0]}</div>
+                <div className="logAddress">{data.address}</div>
+                <div className="logTheme">{data.theme}</div>
+              </div>
+              {/* 색상 좀 연하게 + border-bottom로 변경 */}
+              <div className="divider"></div>
             </div>
-            {/* 색상 좀 연하게 + border-bottom로 변경 */}
-            <div className="divider"></div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
       <div className="logContent">
         <section id="logMapContainer" ref={containerRef} className="kakaoMap" />
