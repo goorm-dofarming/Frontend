69b2263d12ddbe4a2eaae6a54d07167a257dbe74
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-08-09 23:47:44 +0900

    build: 빌드 로그 추적 및 BeforeInstall 로직 삭제

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,14 +6,14 @@ REPOSITORY=/home/ubuntu/deploy
 cd $REPOSITORY
 
 # Install packages
-pnpm install --force
+pnpm install
 
 # 권한 부여
 sudo chown -R ubuntu:ubuntu /home/ubuntu/deploy/.next
 sudo chmod -R 755 /home/ubuntu/deploy/.next
 
 # build
-pnpm build
+pnpm build --verbose | tee build_log.txt
 
 # Start or restart the application
 sudo pm2 resurrect || sudo pm2 start npm --name "Frontend" -- start
