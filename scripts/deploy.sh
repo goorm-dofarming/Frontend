e086a04cd9f4294c14ce7120757637ee95e94e06
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-08-11 00:29:17 +0900

    build: 배포

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 # set -e
 
-LOG_FILE="/var/log/deploy.log"
-
-# 로그 파일이 없다면 생성
-if [ ! -f "$LOG_FILE" ]; then
-    sudo touch "$LOG_FILE"
-    sudo chown ubuntu:ubuntu "$LOG_FILE"
-    sudo chmod 644 "$LOG_FILE"
-fi
-
-echo "$(date): Deploy script is starting..." >> $LOG_FILE
 
 # 권한 부여
-echo "Changing ownership and permissions..." >> $LOG_FILE
+echo "Changing ownership and permissions..."
 sudo chown -R ubuntu:ubuntu /home/ubuntu/deploy
 sudo chmod -R 755 /home/ubuntu/deploy
 
@@ -21,11 +11,11 @@ sudo chmod -R 755 /home/ubuntu/deploy
 cd /home/ubuntu/deploy
 
 # 패키지 설치
-echo "Installing packages..." >> $LOG_FILE
+echo "Installing packages..."
 pnpm install
 
 # 애플리케이션 시작 또는 재시작
-echo "Starting or resurrecting the application..." >> $LOG_FILE
+echo "Starting or resurrecting the application..."
 sudo pm2 resurrect || sudo pm2 start npm --name "Frontend" -- start
 
-echo "Deployment finished successfully." >> $LOG_FILE
+echo "Deployment finished successfully."
