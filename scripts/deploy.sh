a8c2ef27003fb8c2c687116069c1f3576551ae72
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-08-10 13:52:55 +0900

    build: 배포

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,15 +5,15 @@ REPOSITORY=/home/ubuntu/deploy
 
 cd $REPOSITORY
 
+# 권한 부여
+sudo chown -R ubuntu:ubuntu /home/ubuntu/deploy/.next
+sudo chmod -R 755 /home/ubuntu/deploy/.next
+
 # 기존 node_modules 삭제
 rm -rf node_modules pnpm-lock.yaml
 
 # Install packages
 pnpm install
-
-# 권한 부여
-sudo chown -R ubuntu:ubuntu /home/ubuntu/deploy/.next
-sudo chmod -R 755 /home/ubuntu/deploy/.next
 
 # build steps
 pnpm build --verbose | tee build_log.txt
