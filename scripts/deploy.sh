9f1c86e7f98e5da69cedd9718860fa3c29349073
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-08-10 17:36:50 +0900

    build: 충돌 나는 파일 삭제 로직 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,7 +20,11 @@ pnpm store prune >> error_log.txt 2>&1
 
 # 기존 node_modules 및 관련 디렉터리 삭제
 echo "Removing existing node_modules and build directories..." >> error_log.txt 2>&1
-rm -rf node_modules pnpm-lock.yaml .next >> error_log.txt 2>&1
+sudo rm -rf node_modules pnpm-lock.yaml .next >> error_log.txt 2>&1
+
+# 특정 충돌 디렉토리 삭제 추가
+echo "Removing problematic directory..." >> error_log.txt 2>&1
+sudo rm -rf /home/ubuntu/deploy/node_modules/.pnpm/next@14.2.4_react-dom@18.3.1_react@18.3.1__react@18.3.1_sass@1.77.8/node_modules/next/experimental >> error_log.txt 2>&1
 
 # 패키지 설치
 echo "Installing packages..." >> error_log.txt 2>&1
