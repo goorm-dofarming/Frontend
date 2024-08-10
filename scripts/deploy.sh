c91a607e1d9fe53aafabdd4c044e7bc8345b577b
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-08-10 23:09:41 +0900

    build: deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-set -e  # 에러 발생 시 스크립트 중단
-
-REPOSITORY=/home/ubuntu/deploy
+# set -e
 
 LOG_FILE="/var/log/deploy.log"
 
 echo "$(date): Deploy script is starting..." >> $LOG_FILE
 
 # 권한 부여
-echo "Changing ownership and permissions..."
+echo "Changing ownership and permissions..." >> $LOG_FILE
 sudo chown -R ubuntu:ubuntu /home/ubuntu/deploy
 sudo chmod -R 755 /home/ubuntu/deploy
 
@@ -16,33 +14,31 @@ sudo chmod -R 755 /home/ubuntu/deploy
 cd /home/ubuntu/deploy
 
 # 로그 파일 초기화
-echo "Deploy script started" > error_log.txt  # 로그 파일을 초기화합니다.
+echo "Deploy script started" >> $LOG_FILE  # 로그 파일을 초기화합니다.
 
 # 캐시 정리
-echo "Cleaning pnpm store..." >> error_log.txt 2>&1
-sudo pnpm store prune >> error_log.txt 2>&1
-
+echo "Cleaning pnpm store..." >> $LOG_FILE
+sudo pnpm store prune
 # 기존 node_modules 및 관련 디렉터리 삭제
-echo "Removing existing node_modules and build directories..." >> error_log.txt 2>&1
-sudo rm -rf node_modules >> error_log.txt 2>&1
+echo "Removing existing node_modules and build directories..." >> $LOG_FILE
+sudo rm -rf node_modules
 
-# node_modules 삭제 확인s
+# node_modules 삭제 확인
 if [ ! -d "node_modules" ]; then
-    echo "node_modules directory successfully deleted." >> error_log.txt 2>&1
+    echo "node_modules directory successfully deleted." >> $LOG_FILE
 else
-    echo "Failed to delete node_modules directory." >> error_log.txt 2>&1
+    echo "Failed to delete node_modules directory." >> $LOG_FILE
 fi
 
 # 패키지 설치
-echo "Installing packages..." >> error_log.txt 2>&1
-pnpm install >> error_log.txt 2>&1
+echo "Installing packages..." >> $LOG_FILE
+pnpm install
 
 # 빌드 단계
-echo "Building the project..." >> error_log.txt 2>&1
-pnpm build --verbose | tee build_log.txt >> error_log.txt 2>&1
+echo "Building the project..." >> $LOG_FILE
+pnpm build --verbose | tee build_log.txt
 
 # 애플리케이션 시작 또는 재시작
-echo "Starting or resurrecting the application..." >> error_log.txt 2>&1
-sudo pm2 resurrect >> error_log.txt 2>&1 || sudo pm2 start npm --name "Frontend" -- start >> error_log.txt 2>&1
-
-echo "Deployment finished successfully." >> error_log.txt 2>&1
+echo "Starting or resurrecting the application..." >> $LOG_FILE
+sudo pm2 resurrect || sudo pm2 start npm --name "Frontend" -- start
+echo "Deployment finished successfully." >> $LOG_FILE
