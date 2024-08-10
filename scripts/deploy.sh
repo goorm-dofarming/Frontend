950b2ae5381b9ac9b72d869ce0bd305aec75c457
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-08-11 02:38:17 +0900

    build: node_modules 제대로 설치 되는지 확인 로직 추가 및 로그 파일 생성

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
 set -e
 
-LOG_FILE="/path/to/deploy.log"
+LOG_FILE="/home/ubuntu/deploy/install.log"
 
 # 권한 부여
-echo "Changing ownership and permissions..." >> "$LOG_FILE" 2>&1
+echo "Changing ownership and permissions..." | tee -a $LOG_FILE
 sudo chown -R ubuntu:ubuntu /home/ubuntu/deploy
 sudo chmod -R 755 /home/ubuntu/deploy
 
 # 작업 디렉토리로 이동
 cd /home/ubuntu/deploy
 
-# 패키지 설치
-echo "Installing packages..." >> "$LOG_FILE" 2>&1
-pnpm install --force
-if [ $? -eq 0 ]; then
-    echo "Packages installed successfully." >> "$LOG_FILE" 2>&1
-else
-    echo "Package installation failed." >> "$LOG_FILE" 2>&1
+# 패키지 설치 함수 정의
+install_packages() {
+    echo "Installing packages..." | tee -a $LOG_FILE
+    pnpm install --force | tee -a $LOG_FILE
+
+    if [ $? -eq 0 ]; then
+        echo "Packages installed successfully." | tee -a $LOG_FILE
+        return 0
+    else
+        echo "Package installation failed. Retrying..." | tee -a $LOG_FILE
+        pnpm install --force | tee -a $LOG_FILE
+        return $?
+    fi
+}
+
+# 패키지 설치 시도
+install_packages
+if [ $? -ne 0 ]; then
+    echo "Packages failed to install after retrying." | tee -a $LOG_FILE
     exit 1
 fi
 
 # 애플리케이션 시작 또는 재시작
-echo "Starting or resurrecting the application..." >> "$LOG_FILE" 2>&1
-sudo pm2 resurrect || sudo pm2 start npm --name "Frontend" -- start
+echo "Starting or resurrecting the application..." | tee -a $LOG_FILE
+sudo pm2 resurrect || sudo pm2 start npm --name "Frontend" -- start | tee -a $LOG_FILE
 
-echo "Deployment finished successfully." >> "$LOG_FILE" 2>&1
+echo "Deployment finished successfully." | tee -a $LOG_FILE
