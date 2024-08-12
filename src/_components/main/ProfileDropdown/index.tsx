7c45d8212cbcc953f56565e5fcf049a458ce4bb8
NS0H <bory1610@gmail.com>
2024-08-12 17:47:50 +0900

    refactor: console.log 삭제

--- src/_components/main/ProfileDropdown/index.tsx
+++ src/_components/main/ProfileDropdown/index.tsx
@@ -141,7 +141,7 @@ const ProfileDropdown = ({
     if (id === 'settings') {
       openModal();
     } else {
-      console.log('randomPin:', randomPin);
+      // console.log('randomPin:', randomPin);
       if (randomPin.logId === 0 && (id === 'map' || id === 'log')) {
         if (openToast) {
           openToast();
