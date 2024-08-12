2eff99ee2f1749f11c26178aa1490f1a9ad77f54
NS0H <bory1610@gmail.com>
2024-08-12 16:04:07 +0900

    fix: 채팅 관련 오류 수정

--- src/_components/main/chat/chatList/MyChatList.tsx
+++ src/_components/main/chat/chatList/MyChatList.tsx
@@ -162,9 +162,9 @@ const MyChatList: React.FC<MyChatListProps> = ({ myChatQuery }) => {
     }
   }, [selectedChat]);
 
-  useEffect(() => {
-    console.log('messageAlarm: ', messageAlarm);
-  }, [messageAlarm]);
+  // useEffect(() => {
+  //   console.log('messageAlarm: ', messageAlarm);
+  // }, [messageAlarm]);
 
   if (loading) {
     return (
